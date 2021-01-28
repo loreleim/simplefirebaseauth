fab6adf7675ae35eb2e50251a60df4260ce2bcc5
loreleim <lorelei.miyamura@gmail.com>
2021-01-27 18:06:20 -0800

    namefixes on the login component

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,4 @@
-function App() {
+function Login() {
   return (
     <div className="App">
       <h1>Login</h1>
@@ -6,4 +6,4 @@ function App() {
   );
 }
 
-export default App;
+export default Login;
